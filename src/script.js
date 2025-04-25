70b6ac2793c17d58e56f2dda7d336d36056ac4ed
PxMach <moundjidmachghour58@gmail.com>
2025-04-25 16:19:50 +0100

    Ajout d'une icône de menu dans le HTML, mise à jour du script JavaScript pour gérer l'affichage du menu, et modifications du style CSS pour améliorer la présentation du menu et de la navigation.

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
 document.getElementById("nav").addEventListener("click", function () {
    const menu = document.getElementById("menu");
+   const img = document.getElementById("img");
    if (menu.style.display === "none") {
       menu.style.display = "block";
    } else {
       menu.style.display = "none";
    }
-   //    menu.style.display = menu.style.display === "block" ? "none" : "block";
 });
