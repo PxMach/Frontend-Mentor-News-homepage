964674a668340fe33aeff4e7dc4edd26603a4538
PxMach <moundjidmachghour58@gmail.com>
2025-04-29 10:30:31 +0100

    Ajout d'une classe au corps dans le HTML, mise à jour du script JavaScript pour gérer l'opacité du corps lors de l'ouverture et de la fermeture du menu, et ajustement des styles CSS pour le menu et la navigation sur les écrans de petite taille.

--- src/script.js
+++ src/script.js
@@ -2,12 +2,16 @@ const nav = document.getElementById("nav");
 const menu = document.getElementById("menu");
 const openMenu = document.getElementById("open-menu");
 const closeMenu = document.getElementById("close-menu");
+const body = document.querySelector(".body");
 
 function openClick() {
-   if (nav.style.display === "none") {
+   if (nav.style.display === "none" || nav.style.display === "") {
+      body.style.opacity = 0.6;
       nav.style.display = "block";
+      nav.style.opacity = 1;
    } else {
       nav.style.display = "none";
+      body.style.opacity = 1;
    }
 }
 
@@ -16,6 +20,7 @@ openMenu.addEventListener("click", openClick);
 function closeClick() {
    if (nav.style.display === "block") {
       nav.style.display = "none";
+      body.style.opacity = 1;
    } else {
       nav.style.display = "block";
    }
